57dbc6c317cf167c5e7362b5e6e349c334d5d740
aadilKhan <aadil.outworktech@gmail.com>
2026-05-19 17:38:53 +0530

    Fix: remove incompatible dynamic-import babel plugin, Metro stub is sufficient

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Transform dynamic import() to require() — Hermes doesn't support import()
-      "@babel/plugin-transform-dynamic-import",
-    ],
   };
 };
